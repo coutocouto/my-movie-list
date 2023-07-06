c074c77d49d7b9128461439d5a3d53ab82c63e3b
Felipe Couto <couto.felipe2@gmail.com>
2023-07-06 00:08:43 -0300

    teste

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -21,7 +21,7 @@ const NotFound = () => {
             </h6>
             <p className="text-gray-500 mt-3">{" The page you're looking for dosen't exist."}</p>
             <Link href="/">
-                <a className="bg-blue-100 text-blue-800 mt-5 font-semibold px-6 py-2 text-sm">Go home</a>
+                <a className="bg-blue-100 text-blue-800 mt-5 font-semibold px-6 py-2 text-sm">Go home!</a>
             </Link>
         </div>
     );
